f1ab7eb573a7f344ae10ec850020964f14747de9
Frank Combopiano <frank.combopiano@gmail.com>
2016-05-10 10:24:55 -0400

    Added whois to provisioning.sh; created mendix_user with set password

--- provisioning.sh
+++ provisioning.sh
@@ -8,10 +8,11 @@ echo "Installing common items and ansible dependencies"
 sudo apt-get install curl unzip software-properties-common python-dev python-setuptools -y
 sudo easy_install pip
 
-echo "Installing ansible"
+echo "Installing ansible and whois (for mkpasswd)"
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
+sudo apt-get install whois -y
 
 echo "Installing ansible dependencies"
 /usr/local/bin/pip install git+git://github.com/openstack/python-novaclient
